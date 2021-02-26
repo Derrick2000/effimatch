42612c5a719aa3e15c9d81fb813cdf9117eff8ea
jinghongluo <jil040@ucsd.edu>
2021-02-25 20:47:49 -0800

    skeleton fix & basic page

--- frontend/src/screens/SignIn/SignIn.tsx
+++ frontend/src/screens/SignIn/SignIn.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import axios from 'axios'
 
+import {ReactComponent as SignUpBackGround} from '../../images/sign_bg.svg';
+import {ReactComponent as SignInPerson} from '../../images/sign_in_person.svg';
+
 import { Input, Button } from 'antd';
+
+import './styles/signin.less';
 
 const SignIn: React.FC<any> = (props) => {
 
@@ -21,19 +26,40 @@ const SignIn: React.FC<any> = (props) => {
         })
     }
 
+    const signWithLinkedIn = () => {
+
+    }
+
     return(
-        <div>
-            <p>email</p>
-            <Input 
-                onChange={e => setEmail(e.target.value)}
-                value={email}
-            />
-            <p>password</p>
-            <Input.Password 
-                onChange={e => setPassword(e.target.value)}
-                value={password}
-            />
-            <Button onClick={e => login()}>Log In</Button>
+        <div className="signin-wrapper">
+
+            <SignUpBackGround className="signin-wrapper-bg"/>
+            
+
+            <div className="signin-box">
+                <h1 className="signin-box-header">
+                    Log in to access thousands of potential referral opportunities.
+                </h1>
+                <div className="signin-box-form">
+                    <p className="signin-box-form-text">email</p>
+                    <Input 
+                        onChange={e => setEmail(e.target.value)}
+                        value={email}
+                        className="signin-box-form-input"
+                    />
+                    <p className="signin-box-form-text">password</p>
+                    <Input.Password 
+                        onChange={e => setPassword(e.target.value)}
+                        value={password}
+                        className="signin-box-form-input"
+                    />
+                    <Button type="primary" className="signin-box-signinBut" onClick={e => login()}>Log In</Button>
+                    <Button type="primary" className="signin-box-linkedinBut" onClick={e=> signWithLinkedIn()}>Continue with LinkedIn</Button>
+                </div>
+            </div>
+
+
+            <SignInPerson className="signin-wrapper-person"/>
         </div>
     )
 }
