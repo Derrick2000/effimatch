96c93c8bce01367601bbfcb14e2e9560235744c7
Dekun Ma <dekun.ma@thoughtworks.com>
2021-07-13 19:44:07 +0800

    [TECH][DEKUN]Changed home card redirection logic

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,34 +1,47 @@
-import React from 'react'
-import { Button } from 'antd/lib'
+import React from 'react';
+import {Button} from 'antd/lib';
 import './cardStyle.less';
+import {useSelector} from 'react-redux';
 
 interface CardProps {
-    title: string,
-    company: string,
-    logo: string,
-    avatar: string,
-    name: string,
+  title: string;
+  company: string;
+  logo: string;
+  avatar: string;
+  name: string;
 }
 
-const Card:React.FC<CardProps> = (props: CardProps) => {
-    return (
-        <a className='card-block-group' href='/post-details'>
-            <div className='card-header-wrapper'>
-                <div className='card-header-title'>
-                    <h1>{props.title}</h1>
-                    <p>@ {props.company}</p>
-                </div>
-                <img src={props.logo} alt='logo' />
-            </div>
+type authState = {
+  isAuthenticated: boolean;
+};
 
-            <div className='card-avatar-wrapper'>
-                <img src={props.avatar} alt='avatar' />
-                <p className='card-avatar-text'>{props.name}</p>
-            </div>
+const Card: React.FC<CardProps> = (props: CardProps) => {
+  const auth: authState = useSelector((state: any) => state.auth);
 
-            <Button type='primary' className='card-button'>Get Referral</Button>
-        </a>
-    )
-}
+  const getRedirectPath = () => {
+    return auth.isAuthenticated ? '/post-details' : '/sign-in';
+  };
+
+  return (
+    <a className="card-block-group" href={getRedirectPath()}>
+      <div className="card-header-wrapper">
+        <div className="card-header-title">
+          <h1>{props.title}</h1>
+          <p>@ {props.company}</p>
+        </div>
+        <img src={props.logo} alt="logo" />
+      </div>
+
+      <div className="card-avatar-wrapper">
+        <img src={props.avatar} alt="avatar" />
+        <p className="card-avatar-text">{props.name}</p>
+      </div>
+
+      <Button type="primary" className="card-button">
+        Get Referral
+      </Button>
+    </a>
+  );
+};
 
 export default Card;
