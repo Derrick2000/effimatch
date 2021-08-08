8350ab741cc4a007112d5824c73687506eec1bd9
Dekun Ma <dekun.ma@thoughtworks.com>
2021-08-08 21:30:31 +0800

    [TECH][DEKUN]Changed URL to local

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,3 @@
-export const baseURL = 'https://effimatch-server.ucsdtriplec.cn';
+// export const baseURL = 'https://effimatch-server.ucsdtriplec.cn';
+export const baseURL = 'http://localhost:8080';
 export const clearbitKey = 'sk_3c112e57c8aca581cd3005cb40c5af86';
