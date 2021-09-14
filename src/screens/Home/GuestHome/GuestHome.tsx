29531ac93f1f10a89b5ee1ae5e0ed8dcec530960
dekunma <William@dekun.me>
2021-09-14 10:29:59 -0400

    [TECH][DEKUN]Removed test comment

--- src/screens/Home/GuestHome/GuestHome.tsx
+++ src/screens/Home/GuestHome/GuestHome.tsx
@@ -19,8 +19,6 @@ import MS_logo from 'images/MS_logo.png';
 
 import '../styles/home.less';
 
-// test husky
-
 const RenderCards = (cardsData?: JobCardResponse[]) => {
   return (
     <div className="home-cards-wrapper">
